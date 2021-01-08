43f79c3315a008e7c33faded3dbf61c5ca2d1f58
dorotheeOC <69916060+dorotheeOC@users.noreply.github.com>
2021-01-08 20:23:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://dslite:motdepasse@cluster0.oxj94.mongodb.net/test?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://validateur:motdepasse@cluster0.oxj94.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
