7e82a9294c8cf3a77a3fde52d20d76acc503bf2d
dorotheeOC <ds.lite45@gmail.com>
2021-04-18 14:58:35 +0200

    brute force

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const rateLimit = require("express-rate-limit");
+
+const accountLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 1 hour
+    max: 3, // limit each IP to 6 requests per windowMs
+    message: "Trop de tentative, réessayer dans 15 minutes"
+});
 
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login)
+router.post('/login', accountLimiter, userCtrl.login)
 
 module.exports = router;
